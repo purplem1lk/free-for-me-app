730c5ce6efa183d2fad1fb0ecb578587f98a318c
Susan Ma <masusan29@gmail.com>
2019-10-30 11:35:13 -0400

    Add Material UI, add mobile screen capability, edit navigation bar, edit listings index page css, edit README.md, update sign-in form

--- app/javascript/react/components/ListingTile.js
+++ app/javascript/react/components/ListingTile.js
@@ -1,7 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    height: 200
+  },
+  title: {
+    fontSize: 16
+  },
+  media: {
+    height: 140
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 
 const ListingTile = props => {
+  const classes = useStyles();
+
   let image = props.image;
 
   if (!image) {
@@ -9,17 +35,21 @@ const ListingTile = props => {
   }
 
   return (
-    <div className="columns small-12 medium-6 large-4">
-      <div className="container listing-tile-info image-container">
+    <div className="columns small-12 medium-6 large-4 foundation-div">
+      <CardActionArea>
         <Link to={`/listings/${props.id}`}>
-          <img className="listing-tile-image" src={image} />
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image="https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3"
+              title="Listing Tile"
+            />
+            <CardContent>
+              <Typography className={classes.title}>{props.title}</Typography>
+            </CardContent>
+          </Card>
         </Link>
-        <div className="bottom-centered">
-          <p className="listing-tile-info">
-            {props.title}, {props.postal_code}
-          </p>
-        </div>
-      </div>
+      </CardActionArea>
     </div>
   );
 };
