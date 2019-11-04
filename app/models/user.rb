acc67845b732581e83fdd23fc48de5972c5ce01e
Susan Ma <masusan29@gmail.com>
2019-11-03 20:18:19 -0500

    Update seeds file, update submit buttons, edit user model to exclude photo uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  mount_uploader :profile_photo, ProfilePhotoUploader
-
   validates :username, presence: true
   validates :password, length: { minimum: 6 }, on: :create
 
